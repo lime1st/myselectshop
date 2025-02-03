bf02c54cd96e7eb54e09264293276e717ab06f5a
lime1st <lime1st@gmail.com>
2025-02-03 18:10:57 +0900

    init

---
+++ src/test/java/lime1st/myselectshop/MyselectshopApplicationTests.java
@@ -0,0 +1,13 @@
+package lime1st.myselectshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyselectshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
