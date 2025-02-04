235e4ed116d2177db86eaf5f9ae9d5afb9855b9c
lime1st <lime1st@gmail.com>
2025-02-04 12:13:42 +0900

    상품 폴더 설계

---
+++ src/main/java/lime1st/myselectshop/repository/FolderRepository.java
@@ -0,0 +1,7 @@
+package lime1st.myselectshop.repository;
+
+import lime1st.myselectshop.entity.Folder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FolderRepository extends JpaRepository<Folder, Long> {
+}
---
+++ src/main/java/lime1st/myselectshop/repository/ProductFolderRepository.java
@@ -0,0 +1,7 @@
+package lime1st.myselectshop.repository;
+
+import lime1st.myselectshop.entity.ProductFolder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductFolderRepository extends JpaRepository<ProductFolder, Long> {
+}
